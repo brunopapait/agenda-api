caf11b85c77fd6a23e3462b2182d55972accda2c
brunopapait <brunohpapait@gmail.com>
2023-07-19 23:41:35 -0300

    project init

---
+++ src/test/java/br/com/papait/bruno/agenda/api/AgendaApiApplicationTests.java
@@ -0,0 +1,8 @@
+package br.com.papait.bruno.agenda.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApiApplicationTests {
+}
